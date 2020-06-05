46debeea00a5ad3f04c6075d3d3375ab1572f625
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-06-05 13:22:17 +0100

    Add rudimentary fixture tests (#9)

--- tests/index.ts
+++ tests/index.ts
@@ -1,5 +1,70 @@
+import * as fixtures from '../src'
+
 describe('fixtures', () => {
-  test('it passes', () => {
-    expect(true).toBeTruthy()
+  test('application', () => {
+    const owner = fixtures.user({ id: 1, name: 'github' })
+    const app = fixtures.application({
+      id: 1,
+      name: 'Hello world',
+      owner,
+      permissions: {
+        metadata: 'read',
+      },
+      events: ['push'],
+    })
+
+    expect(app).toMatchObject({ id: 1, name: 'Hello world', slug: 'hello-world' })
+  })
+
+  test('installation', () => {
+    const owner = fixtures.user({ id: 1, name: 'github' })
+    const app = fixtures.application({
+      id: 1,
+      name: 'Hello world',
+      owner,
+      permissions: {
+        metadata: 'read',
+      },
+      events: ['push'],
+    })
+    const inst = fixtures.installation({ id: 1, application: app, account: owner })
+
+    expect(inst).toMatchObject({ id: 1, account: owner })
+  })
+
+  test('organization', () => {
+    const org = fixtures.organization({ id: 1, name: 'github' })
+
+    expect(org).toMatchObject({ id: 1, name: 'github', login: 'github' })
+  })
+
+  test('repository', () => {
+    const owner = fixtures.user({ id: 1, name: 'github' })
+    const repo = fixtures.repository({ id: 1, name: 'hello', owner })
+
+    expect(repo).toMatchObject({ id: 1, name: 'hello', owner, full_name: 'github/hello' })
+  })
+
+  test('token', () => {
+    const owner = fixtures.user({ id: 1, name: 'github' })
+    const repo = fixtures.repository({ id: 1, name: 'hello-world', owner })
+    const app = fixtures.application({
+      id: 1,
+      name: 'Hello world',
+      owner,
+      permissions: {
+        metadata: 'read',
+      },
+      events: ['push'],
+    })
+    const token = fixtures.token({ value: 'test', application: app, repositories: [repo] })
+
+    expect(token).toMatchObject({ token: 'test', repositories: [repo] })
+  })
+
+  test('user', () => {
+    const user = fixtures.user({ id: 1, name: 'github' })
+
+    expect(user).toMatchObject({ id: 1, login: 'github' })
   })
 })
