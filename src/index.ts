368fc38f345847229c484e7997e7c1f669cbef04
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-06-05 12:52:01 +0100

    Add application fixture (#6)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { application, Application } from './types/application'
 export { organization, Organization } from './types/organization'
 export { repository, Repository } from './types/repository'
 export { user, User } from './types/user'
