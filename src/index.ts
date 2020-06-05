403c8ed07a7c0d7566edb0f971f927dffb28f5c4
Daniel Balcomb <daniel.balcomb@gmail.com>
2020-06-05 12:56:23 +0100

    Add installation fixture

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { application, Application } from './types/application'
+export { installation, Installation } from './types/installation'
 export { organization, Organization } from './types/organization'
 export { repository, Repository } from './types/repository'
 export { user, User } from './types/user'
